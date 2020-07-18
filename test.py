a703358a97ac96c0869c552762c5f42f713b14c5
Borislav Varadinov <bvaradinov-c@hedgeserv.com>
2020-07-18 09:21:53 +0300

    test

---
+++ test.py
@@ -0,0 +1,7 @@
+import os
+import requests
+
+endpoint = os.getenv('AWS_CONTAINER_CREDENTIALS_RELATIVE_URI')
+creds = requests.get(f'http://169.254.170.2{endpoint}')
+print(creds['AccessKeyId'])
+print(creds['SecretAccessKey'])
